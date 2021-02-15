b4093c28e76e10258d68b31884a99c7c9c77f3d0
Dustin Fox <dustinfox1996@gmail.com>
2021-02-15 15:38:17 -0500

    Add project files.

---
+++ Cellphone/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Cellphone {
+    class Program {
+        static void Main(string[] args) {
+            var cp1 = new Cellphone {
+                PhoneNumber = "513-331-1710",
+                ServiceProvider = "T-Mobile",
+                OperatingSystem = "iOS",
+                Capacity = 128,
+                Model = "iPhone X"
+            };
+            var cp2 = new Cellphone {
+                PhoneNumber = "513-410-5301",
+                ServiceProvider = "T-Mobile",
+                OperatingSystem = "Android",
+                Capacity = 64,
+                Model = "Samsung"
+            };
+            var cellPhones = new Cellphone[] { cp1, cp2 };
+
+            foreach( var cp in cellPhones) {
+                Console.WriteLine($"phone number is {cp.PhoneNumber}");
+            }
+        }
+    }
+    
+}
